23d6eeee00218564ec31f47fd4b04242427de43a
BhoneMyatOo17 <bhonemyatoo73@gmail.com>
2026-02-24 11:39:46 +0700

    Condo Seeder Fix

--- database/factories/CondominiumFactory.php
+++ database/factories/CondominiumFactory.php
@@ -10,6 +10,8 @@ class CondominiumFactory extends Factory
     protected $model = Condominium::class;
     public function definition(): array
     {
+        static $nameIndex = 0;
+
         $names = [
             'Ideo Q Sukhumvit 36',
             'Ideo Rama 9 - Asoke',
@@ -21,18 +23,19 @@ class CondominiumFactory extends Factory
             'Ideo Blucove Sukhumvit',
         ];
 
-        $name = $this->faker->unique()->randomElement($names);
+        $name = $names[$nameIndex % count($names)];
+        $nameIndex++;
 
         return [
             'name' => $name,
-            'code' => 'IDO' . $this->faker->unique()->numberBetween(100, 999),
-            'address' => $this->faker->streetAddress() . ', Bangkok ' . $this->faker->postcode() . ', Thailand',
-            'phone_number' => '02-' . $this->faker->numberBetween(100, 999) . '-' . $this->faker->numberBetween(1000, 9999),
-            'email' => 'juristic.' . strtolower(str_replace(' ', '', explode(' ', $name)[1] ?? $this->faker->word())) . '@ideo.co.th',
-            'line_id' => '@ideo' . strtolower($this->faker->lexify('???')),
-            'total_floors' => $this->faker->numberBetween(20, 50),
-            'total_units' => $this->faker->randomElement([200, 300, 400, 500, 600]),
-            'built_year' => $this->faker->numberBetween(2015, 2024),
+            'code' => 'IDO' . fake()->unique()->numberBetween(100, 999),
+            'address' => fake()->streetAddress() . ', Bangkok ' . fake()->postcode() . ', Thailand',
+            'phone_number' => '02-' . fake()->numberBetween(100, 999) . '-' . fake()->numberBetween(1000, 9999),
+            'email' => 'juristic.' . strtolower(str_replace(' ', '', explode(' ', $name)[1] ?? fake()->word())) . '@ideo.co.th',
+            'line_id' => '@ideo' . strtolower(fake()->lexify('???')),
+            'total_floors' => fake()->numberBetween(20, 50),
+            'total_units' => fake()->randomElement([200, 300, 400, 500, 600]),
+            'built_year' => fake()->numberBetween(2015, 2024),
         ];
     }
 }
